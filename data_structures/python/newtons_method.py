bacf39ce31fc9a714eb27cfe6d936466bba5c558
alex <alexanderpadron@stmarksschool.org>
2016-08-15 15:56:09 -0400

    implemented exp

--- data_structures/python/newtons_method.py
+++ data_structures/python/newtons_method.py
@@ -1,8 +1,10 @@
-
+from hashtbl import Hashtbl
 
 def divide_by_power_of_two(x, exp):
     return x >> exp
     
+def is_even(x):
+    return x == (x >> 1) << 1
 def find_closest_power_of_two(x):
     closest_value = 1
     p = 0
@@ -14,11 +16,33 @@ def find_closest_power_of_two(x):
     else:
         return p + 1
 
+def exp(a, b):
+    dp_store = Hashtbl(4)
+    def f(a, b):
+        #if we have already computed the result for this exponent, return it
+        if dp_store.get(str(b)) != None:
+            return dp_store.get(str(b))
+        if b == 1:
+            return a
+        if b == 0:
+            return 1
+        if is_even(b):
+            b =  divide_by_power_of_two(b, 1)
+            result = f(a*a, b)
+            dp_store.set(str(b), result)
+            return result
+        else:
+            b = divide_by_power_of_two(b-1, 1)
+            result = a*f(a*a, b)
+            dp_store.set(str(b), result)
+            return result
+    return f(a, b)
+    
 #simple implementation of divide that doesn't use floating point
 R = 30
 def div(a, b):
     #initial guess
-    x = divide_by_power_of_two(2**R, find_closest_power_of_two(b))
+    x = divide_by_power_of_two(exp(R,2), find_closest_power_of_two(b))
     #update with newton's method
     y = -1
     while y != x:
@@ -39,11 +63,26 @@ def unit_test():
     assert(find_closest_power_of_two(15) == 4)
     assert(find_closest_power_of_two(65) == 6)
     print("...passed")
-    
+
+    print("testing is_even...")
+    assert(is_even(2))
+    assert(not is_even(3))
+    assert(is_even(0))
+    assert(not is_even(27))
+    print("...passed")
+
+    print("testing exp...")
+    assert(exp(3,2) == 9)
+    assert(exp(1,1) == 1)
+    assert(exp(3,0) == 1)
+    assert(exp(0,1) == 0)
+    assert(exp(2,10) == 1024)
+    print("...passed")
+
     print("testing div...")
     assert(div(4,2) == 2)
     assert(div(423, 76) == 5)
-    assert(div(423423452454353, 42342424) == 10252939)
+    assert(div(400*760, 760) == 400)
     print("...passed")
 if __name__ == "__main__":
     unit_test()
