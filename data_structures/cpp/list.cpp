21f6d3f56e7b52c09d31875a29b7744926843ca3
alex <alexanderpadron@stmarksschool.org>
2016-08-18 12:19:11 -0400

    implemented filter

--- data_structures/cpp/list.cpp
+++ data_structures/cpp/list.cpp
@@ -1,12 +1,8 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <string>
 #include <assert.h>
-#include <iostream>
-#include<sstream>
+#include <memory>
 
 template <class T>
-
 class ListNode {
 public:
   T *data;
@@ -18,19 +14,17 @@ public:
   }
 
   ~ListNode() {
-    printf("deleting node\n");
-    if (this->next != NULL) {
-      printf("passing to next node\n");
-      delete(this->next);
-    }
+    if (this->next != NULL) delete(this->next);
+   
   }
 };
   
 template <class T>
 class List {
-  ListNode<T> *head;
-
 public:
+  typedef ListNode<T>* node;
+  node head;
+  
   List() {
     head = NULL;
   }
@@ -41,16 +35,10 @@ public:
     }
   }
   
-  void push(T *data) {
-    if (head == NULL) {
-      head = new ListNode<T>(data);
-    } else {
-      ListNode<T> *n = head;
-      while (n->next != NULL) {
-	n = n->next;
-      }
-      n->next = new ListNode<T>(data);
-    }
+  void push(T *new_data) {
+    node tmp = head;
+    head = new ListNode<T>(new_data);
+    head->next = tmp;
   }
 
   T *pop() {
@@ -59,24 +47,101 @@ public:
     head = head->next;
     return ret;
   }
+  
+  int length() {
+    int count = 0;
+    node n = head;
+    while (n != NULL) {
+      count += 1;
+      n = n->next;
+    }
+    return count;
+  }
+
+  void filter(bool (*f)(T)) {
+    if (head == NULL) return;
+    //filter head repeatedly
+    while (f(*head->data)) {
+      head = head->next;
+      if (head == NULL) return;
+    }
+    //filter child nodes
+    node n = head;
+    while (n != NULL && n->next != NULL) {
+      if (f(*n->next->data)) {
+	n->next = n->next->next;
+      }
+      n = n->next;
+    }
+  }
 };
 
-template <typename T>
-std::string to_string(T value) {
-  std::ostringstream os;
-  os << value;
-  return os.str();
-}
-
+void list_unit_test();
 int main() {
-  printf("Testing push and pop... \n");
-  List<int> l;
-  for (int i = 0; i < 10; i++) {
-    l.push(&i);
-  }
-  for (int i = 0; i >= 10; i) {
-    assert(*l.pop() == (10 -i));
-  }
-  printf("...passed \n");
+  list_unit_test();
   return 0;
 }
+
+bool is_even(int x) {
+  return x % 2 == 0;
+}
+
+void list_unit_test() {
+  printf("testing push, pop, and length... \n");
+  int x[10];
+  for (int i = 0; i < 10; i++) {
+    x[i] = i;
+  }
+  List<int> l;
+  for (int i = 0; i < 10; i++) {
+    l.push(&x[i]);
+    assert(l.length() == i + 1);
+  }
+  for (int i = 0; i < 10; i++) {
+    assert(*l.pop() == 9 - i);
+    assert(l.length() == 9 - i);
+  }
+  printf("...passed \n");
+
+  printf("testing filter... \n");
+  /*assert(l.length() == 0);
+  int y = 1;
+  int z = 2;
+  l.push(&y);
+  l.push(&z);
+  l.filter(is_even);
+  assert(l.head->next == NULL);
+  assert(*l.head->data == 1);
+  l.pop();
+  assert(l.length() == 0);
+  l.push(&z);
+  l.push(&y);
+  l.filter(is_even);
+  assert(l.head->next == NULL);
+  assert(*l.head->data == 1);
+  l.pop();
+  assert(l.length() == 0);
+
+  int a = 3;
+  int b = 4;
+  l.push(&b);
+  l.push(&a);
+  l.push(&z);
+  l.push(&y);
+  l.filter(is_even);
+  assert(*l.head->data == 1);
+  assert(*l.head->next->data == 3);
+  l.pop();
+  l.pop();
+  assert(l.length() == 0);
+  */
+  for (int i = 0; i < 10; i++) {
+    l.push(&x[i]);
+  }
+  l.filter(is_even);
+  for (int i = 9; i > 0; i = i - 2) {
+    assert(*l.pop() == i);
+  }
+  printf("...passed \n");
+}
+    
