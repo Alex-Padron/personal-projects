1cb52a065fab376a8b8de1c045ce019e86c85c7e
alex <alexanderpadron@stmarksschool.org>
2016-08-23 21:20:56 -0400

    z

--- email_client/windows-main.sh
+++ email_client/windows-main.sh
@@ -6,7 +6,7 @@ END { printf("%s\n",ip); }'`
 echo Current ip is $ip 1>&2 
 echo $ip 
 
-sed 's/<IPADDR>/ip/g' index.js > index-tmp.js
+sed 's/<IPADDR>/$ip/g' index.js > index-tmp.js
 sed 's/index.js/index-tmp.js/g' index.html > index-tmp.html
 echo "writing config for ip addr $ip"
 google-chrome index-tmp.html
