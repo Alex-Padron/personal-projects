f48c0c97fafa308112f8861501e00f2c520cd754
alex <alexanderpadron@stmarksschool.org>
2016-08-23 21:37:12 -0400

    z

--- email_client/windows-main.sh
+++ email_client/windows-main.sh
@@ -7,7 +7,7 @@ echo Updating temp files with ip $IP 1>&2
 
 sed 's/<IPADDR>/'$IP'/g' index.js > index-tmp.js
 sed 's/index.js/index-tmp.js/g' index.html > index-tmp.html
-"C:\Program Files (x86)\Google\Chrome\chrome.exe" index-tmp.html
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" index-tmp.html
 python3 server.py
 
 
