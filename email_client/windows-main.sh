7ba3a3c34c0fa1f2428fa9018e9d97b78cb0c90f
alex <alexanderpadron@stmarksschool.org>
2016-08-23 21:13:38 -0400

    z

--- email_client/windows-main.sh
+++ email_client/windows-main.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+for /f "delims=[] tokens=2" %%a in ('ping -4 %computername% -n 1 ^| findstr "["') do (set thisip=%%a)
+echo %thisip%
+
+
 ip="$(C:\> ipconfig | grep -i 'Ip Address' | grep -v ': 19' | grep -v ': 0.' | grep -v ': 10.' | cut -b44-)"
 sed 's/<IPADDR>/ip/g' index.js > index-tmp.js
 sed 's/index.js/index-tmp.js/g' index.html > index-tmp.html
