9728214e9368d9c03345f79edcd3724dfdf49652
alex <alexanderpadron@stmarksschool.org>
2016-08-23 21:24:44 -0400

    z

--- email_client/windows-main.sh
+++ email_client/windows-main.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-ip=`route print | egrep "^ +0.0.0.0 +0.0.0.0 +" | gawk 'BEGIN { 
+IP=`route print | egrep "^ +0.0.0.0 +0.0.0.0 +" | gawk 'BEGIN { 
 metric=255; ip="0.0.0.0"; } { if ( $5 < metric ) { ip=$4; metric=$5; } } 
 END { printf("%s\n",ip); }'` 
-echo Current ip is $ip 1>&2 
-echo $ip 
+echo Current ip is $IP 1>&2 
 
-sed 's/<IPADDR>/' + $ip + '/g' index.js > index-tmp.js
+sed 's/<IPADDR>/$IP/g' index.js > index-tmp.js
 sed 's/index.js/index-tmp.js/g' index.html > index-tmp.html
 echo "writing config for ip addr $ip"
 google-chrome index-tmp.html
