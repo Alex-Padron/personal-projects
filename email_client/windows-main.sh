f44e2fb787962228ae06880f4bc9f9269a09bcef
alex <alexanderpadron@stmarksschool.org>
2016-08-23 21:29:36 -0400

    z

--- email_client/windows-main.sh
+++ email_client/windows-main.sh
@@ -5,9 +5,8 @@ metric=255; ip="0.0.0.0"; } { if ( $5 < metric ) { ip=$4; metric=$5; } }
 END { printf("%s\n",ip); }'` 
 echo Current ip is $IP 1>&2 
 
-sed 's/<IPADDR>/$IP/g' index.js > index-tmp.js
+sed 's/<IPADDR>/'{$IP}'/g' index.js > index-tmp.js
 sed 's/index.js/index-tmp.js/g' index.html > index-tmp.html
-echo "writing config for ip addr $ip"
 google-chrome index-tmp.html
 python3 server.py
 
